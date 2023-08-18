65e3188fa47c1f14e6f7cd96c47a1a98ec5b00a1
AzeemHashmi <81872529+AzeemHashmi@users.noreply.github.com>
2023-08-18 18:03:13 +0500

    completed-fetching-videos-from-api

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import Sidebar from "./Sidebar";
+import Videos from "./Videos";
+import { fetchFromAPI } from "../utils/FetchFromAPI";
 
 const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&query=${selectedCategory}`).then((data) => setVideos(data.items))
+  }, [selectedCategory]);
+
   return (
     //this stack is for main feed covering sidebar and videos feed
 
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-
       {/* this box is for sidebar */}
 
       <Box
@@ -18,10 +27,31 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <Sidebar/>
-        <Typography className="copyright" variant="body2" sx={{mt:1.5, color:'#fff'}}>
+        <Sidebar 
+          selectedCategory = {selectedCategory}
+          setSelectedCategory = {setSelectedCategory}
+        />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
           Copyright 2023 YT-Media
         </Typography>
+      </Box>
+
+      {/* This box is for videos feed */}
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: "2" }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#F31503" }}>videos</span>
+        </Typography>
+
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
