3740258c834e931c2a42cb7ee0b67587673f0c05
AzeemHashmi <81872529+AzeemHashmi@users.noreply.github.com>
2023-08-29 19:24:13 +0500

    done-with-channelDetail

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -4,8 +4,20 @@ import { CheckCircle } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = ({ channelDetail }) => (
-  <Box sx={{ boxShadow: "none", borderRadius: "20px", display:'flex', justifyContent:'center', alignItems:'center', width:{xs:'356px',md:'320px'}, height:'326px', margin:'auto' }}>
+const ChannelCard = ({ channelDetail, marginTop }) => (
+  <Box
+    sx={{
+      boxShadow: "none",
+      borderRadius: "20px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: { xs: "356px", md: "320px" },
+      height: "326px",
+      margin: "auto",
+      marginTop,
+    }}
+  >
     <Link to={`/channel/${channelDetail?.id?.channelId}`}>
       <CardContent
         sx={{
@@ -20,16 +32,25 @@ const ChannelCard = ({ channelDetail }) => (
           image={
             channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture
           }
-          alt = {channelDetail?.snippet?.title}
-          sx={{borderRadius:'50%', height:'180px', width:'180px', mb:2, border:'1px solid #e3e3e3'}}
+          alt={channelDetail?.snippet?.title}
+          sx={{
+            borderRadius: "50%",
+            height: "180px",
+            width: "180px",
+            mb: 2,
+            border: "1px solid #e3e3e3",
+          }}
         />
         <Typography variant="h6">
           {channelDetail?.snippet?.title}
-          <CheckCircle sx={{fontSize:14, color:'gray', ml:'5px'}} />
+          <CheckCircle sx={{ fontSize: 14, color: "gray", ml: "5px" }} />
         </Typography>
         {channelDetail?.statistics?.subscriberCount && (
           <Typography>
-            {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()} Subscribers
+            {parseInt(
+              channelDetail?.statistics?.subscriberCount
+            ).toLocaleString()}{" "}
+            Subscribers
           </Typography>
         )}
       </CardContent>
